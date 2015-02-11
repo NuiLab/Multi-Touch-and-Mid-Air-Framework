24de33b182730e2e0f2ffc05092c78619b9307ba
aamad011 <aamad011@fiu.edu>
2015-02-11 15:20:18 -0500

    changes

--- Code/TamGestureFramework/src/main.cpp
+++ Code/TamGestureFramework/src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
-using namespace std; 
+#include <data.h>
+#include <vector.h> 
+#include <memory>
+using namespace std;
 int main(int argc, const char * argv[]) {
 	// insert code here...
+	using PVector3 = shared_ptr<TAM::Vector<3, int>>;
+
+	PVector3 myVector; 
+	TAM::Data<PVector3> dd;
+	dd.setData(myVector);
+	TAM::Data<float> d; 
+	//shared_ptr<float> df = make_shared<float>(10);
+	//float * f2 = new float(10); //in c will malloc 
+	d.setData(10.0f);
+
 	cout << "TAM GESTURE FRAMEWORK!\n";
 	return 0;
 }
