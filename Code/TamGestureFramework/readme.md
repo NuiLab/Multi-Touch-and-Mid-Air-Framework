f1ac4869cad688f8b15625d6be4cfb65bd9d2572
aamad011 <aamad011@fiu.edu>
2015-02-11 14:28:39 -0500

    project dir

---
+++ Code/TamGestureFramework/readme.md
@@ -0,0 +1,2 @@
+TAM Gesture Framework 
+GPL v2 and private license available if needed. 
---
+++ Code/TamGestureFramework/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std; 
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	cout << "TAM GESTURE FRAMEWORK!\n";
+	return 0;
+}
