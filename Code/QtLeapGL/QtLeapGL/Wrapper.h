abb3ce26622d9c3b4eef814751dd50ee64153984
Eczeki <vaihtlumin@gmail.com>
2015-12-06 23:54:48 -0500

    Added Project for API

--- Code/QtLeapGL/QtLeapGL/Wrapper.h
+++ Code/QtLeapGL/QtLeapGL/Wrapper.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "api.h"
+#include "api.h" 
 #define SCREEN_SIZE 800 
 
 class Wrapper
--- Code/QtLeapGL/QtLeapGL/main.cpp
+++ Code/QtLeapGL/QtLeapGL/main.cpp
@@ -1,12 +1,19 @@
 #include <QtWidgets/QApplication>
 #include "glwindow.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	
 	GLWindow w;
-	w.show();
+	w.show();	
 	
 	return a.exec();
 }
